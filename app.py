11f502a358cb28bbf6daa3532fb6742ffb78592d
williammccomas <willmc0812@gmail.com>
2026-08-06 00:14:13 -0400

    Added navigation tabs and a basic ranking system

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from spotify_api import get_album, search_albums
 from image_colors import get_dom_color
+from database import save_review, get_reviews
 
 app = Flask(__name__)
 
@@ -9,18 +10,41 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# Album information page route
-@app.route('/album/<album_id>')
+# All rankings page route
+@app.route('/all_rankings')
+def all_rankings():
+    return render_template('all_rankings.html')
+
+# Album ranking form page route
+@app.route('/album/<album_id>', methods=["GET", "POST"])
 def album_review(album_id):
     spotify_data = get_album(album_id)
+    reviews = get_reviews(album_id)
+
+    if request.method == "POST":
+        rating = float(request.form.get("rating"))
+        review = request.form.get("review")
+        save_review(album_id, rating, review)
+
+        return redirect(url_for('ranking', album_id=album_id))
+
     color = get_dom_color(spotify_data['art'])
     color = f"rgb{color}"
-    return render_template('review.html', spotify=spotify_data, color=color)
+    return render_template('ranking_form.html', spotify=spotify_data, color=color, reviews=reviews)
 
-# Rank page route
-@app.route('/rank')
-def rank():
-    return render_template('rank.html')
+# Personal ranking page route
+@app.route('/ranking/<album_id>')
+def ranking(album_id):
+    spotify_data = get_album(album_id)
+    reviews = get_reviews(album_id)
+
+    return render_template('ranking.html', spotify=spotify_data, reviews=reviews)
+    
+
+# Search results page route
+@app.route('/search_results')
+def search_results():
+    return render_template('search_results.html')
 
 # Search page route
 @app.route('/search')
@@ -28,7 +52,7 @@ def search():
     query = request.args.get("query")
 
     if not query or not query.strip():
-        return render_template('rank.html')
+        return render_template('search.html')
 
     results = search_albums(query)
 
---
+++ database.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect('reviews.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS reviews (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            album_id TEXT,
+            rating REAL,
+            review TEXT
+            )""")
+
+    conn.commit()
+    conn.close()
+
+def save_review(album_id, rating, review):
+    conn = sqlite3.connect('reviews.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO reviews (album_id, rating, review) 
+        VALUES (?, ?, ?)
+    """, (album_id, rating, review))
+
+    conn.commit()
+    conn.close()
+
+def get_reviews(album_id):
+    conn = sqlite3.connect('reviews.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT rating, review 
+        FROM reviews 
+        WHERE album_id = ?
+    """, (album_id,))
+    reviews = cursor.fetchall()
+    conn.close()
+    return reviews
+
+def has_reviews(album_id):
+    conn = sqlite3.connect('reviews.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT id 
+        FROM reviews 
+        WHERE album_id = ?
+    """, (album_id,))
+    review = cursor.fetchone()
+    conn.close()
+    return review is not None
+
+if __name__ == "__main__":
+    init_db()
